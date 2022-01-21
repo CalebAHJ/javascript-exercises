0e935c7dc13929b490ba2f4c0bd1bd5a2b9d831b
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-21 14:53:10 -0600

    exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempf) {
+  const tempc = (tempf - 32) * 5 / 9;
+  return +tempc.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(tempc) {
+  const tempf = tempc * 9 / 5 + 32;
+  return +tempf.toFixed(1);
 };
 
 // Do not edit below this line
