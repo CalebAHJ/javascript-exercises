726caf00837f746cd310ec2da2e52b2964d70cf3
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-21 14:22:46 -0600

    exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...nums) {
+    const checkAmount = nums.length;
+    for (let i = 0; i < checkAmount; i++) {
+        const check = array.indexOf(nums.at(i));
+        if (check !== -1) array.splice(check, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
