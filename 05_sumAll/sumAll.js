7deaf5a1ccde2e746595bb8fc9fd7d549e41cb36
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-21 14:33:28 -0600

    exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    
+    let start;
+    let stop;
+    if (num1 <= num2) {
+        start = num1;
+        stop = num2;
+    }
+    else {
+        start = num2;
+        stop = num1;
+    }
+    let sum = 0;
+    for (let i = start; i <= stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
