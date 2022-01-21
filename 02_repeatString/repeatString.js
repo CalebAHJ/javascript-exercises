5d184079069e93125cafaa0f01efb47341fdb253
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-21 13:45:17 -0600

    exercise 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
 
+    let repeatS = "";
+    for (let i = 0; i < num; i++) {
+        repeatS = repeatS + str;
+    }
+    return repeatS;
 };
 
 // Do not edit below this line
