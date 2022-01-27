ea56e59348d16bc41211b3bb6cb0f6ce58fea78d
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-27 12:52:15 -0600

    exercise 09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    const charArray = str.split("");
+    const letterArray = charArray.filter(char => (/[a-z]/).test(char));
 
+    let i1 = 0;
+    let i2 = letterArray.length - 1;
+    while (i1 < i2) {
+        if (letterArray[i1] !== letterArray[i2]) return false;
+        i1++;
+        i2--;
+    }
+    return true;
 };
 
 // Do not edit below this line
