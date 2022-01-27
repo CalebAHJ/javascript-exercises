3cf337a7a332ea85b749863f3cd16d83e0ee0368
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-27 14:14:13 -0600

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((lastPerson, nextPerson) => {
+        if (nextPerson.yearOfDeath == null) {
+            nextPerson.yearOfDeath = (new Date()).getUTCFullYear();
+        }
+        const lastAge = lastPerson.yearOfDeath - lastPerson.yearOfBirth;
+        const nextAge = nextPerson.yearOfDeath - nextPerson.yearOfBirth;
+        return (lastAge > nextAge) ? lastPerson : nextPerson;
+    }, {name: "", yearOfBirth: 0, yearOfDeath: 0});
 };
 
 // Do not edit below this line
