6b6e9e377b16f160c759e33b0f1b4cbd2098082b
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-27 13:02:41 -0600

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(ordinal) {
+    if (typeof ordinal === String) ordinal = +ordinal;
+    if (ordinal < 1) return "OOPS";
+    if (ordinal === 1) return 1;
 
+    let fnum1 = 0;
+    let fnum2 = 1; 
+    for (let i = 1; i < ordinal; i++) {
+        fnum2 = fnum1 + fnum2;
+        fnum1 = fnum2 - fnum1;
+    }
+    return fnum2;
 };
 
 // Do not edit below this line
